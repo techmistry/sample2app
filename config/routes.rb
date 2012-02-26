994482ec93475c63bd17116469808f2814e6def2
Tech Mistry <techmistry@gmail.com>
2012-02-25 21:38:11 -0800

    changes in pages_controller_spec

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Sample2app::Application.routes.draw do
   get "pages/home"
 
   get "pages/contact"
+  
+  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
