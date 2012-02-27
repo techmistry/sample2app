a19da46c9383c5247e5c80f18a0459b2aa629ffc
Tech Mistry <techmistry@gmail.com>
2012-02-26 19:25:03 -0800

    Added a site layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -11,8 +11,14 @@ describe PagesController do
 	
 	it "should have the right title" do
 	  get 'home'
-	  response.should have_selector("title",:content =>"Ruby on Rails Tutorial Sample App | Home") 
+	  response.should have_selector("title",:content =>"| Home") 
 	end
+  
+	it "should have a non-blank body" do
+	  get 'home'
+	  response.body.should_not =~ /<body>\s*<\/body>/
+	end
+  
   end
   
   describe "GET 'contact'" do
@@ -20,6 +26,11 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+	
+	it "should have the right title" do
+	  get 'contact'
+	  response.should have_selector("title",:content =>" | Contact") 
+	end
   end
 
     describe "GET 'about'" do
@@ -27,5 +38,12 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+	
+	it "should have the right title" do
+	  get 'about'
+	  response.should have_selector("title",:content =>" | About") 
+	end
   end
+	
+
 end
